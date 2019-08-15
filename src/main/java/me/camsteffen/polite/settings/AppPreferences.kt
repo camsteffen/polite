b4295bc5029e6aaba414921475381e39e66e0e60
Cameron Steffen <cam.steffen94@gmail.com>
2019-08-15 18:56:49 -0500

    add notification channels

--- src/main/java/me/camsteffen/polite/settings/AppPreferences.kt
+++ src/main/java/me/camsteffen/polite/settings/AppPreferences.kt
@@ -2,6 +2,7 @@ package me.camsteffen.polite.settings
 
 import android.content.SharedPreferences
 import android.content.res.Resources
+import android.os.Build
 import me.camsteffen.polite.R
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -34,7 +35,12 @@ class AppPreferences
         set(value) = editor.putString(themeKey, value).apply()
 
     var notifications: Boolean
-        get() = preferences.getBoolean(notificationsKey, true)
+        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // defer to android notification channels
+            true
+        } else {
+            preferences.getBoolean(notificationsKey, true)
+        }
         set(value) = editor.putBoolean(notificationsKey, value).apply()
 
     var askedToRate: Boolean
--- src/main/java/me/camsteffen/polite/util/AppNotificationManager.kt
+++ src/main/java/me/camsteffen/polite/util/AppNotificationManager.kt
@@ -1,5 +1,6 @@
 package me.camsteffen.polite.util
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
@@ -21,12 +22,36 @@ class AppNotificationManager
     private val context: Context,
     private val notificationManager: NotificationManager
 ) {
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannels()
+        }
+    }
+
     val isNotificationPolicyAccessGranted
         get() = Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
                 notificationManager.isNotificationPolicyAccessGranted
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createChannels() {
+        val politeActiveChannel = NotificationChannel(
+            ChannelIds.POLITE_ACTIVE,
+            context.getString(R.string.polite_active),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(politeActiveChannel)
+
+        val permissionNeededChannel = NotificationChannel(
+            ChannelIds.PERMISSION_NEEDED,
+            context.getString(R.string.permission_needed),
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        notificationManager.createNotificationChannel(permissionNeededChannel)
+    }
+
     fun notifyPoliteActive(text: String) {
-        val notification = NotificationCompat.Builder(context)
+        val notification = NotificationCompat.Builder(context, ChannelIds.POLITE_ACTIVE)
             .setColor(ContextCompat.getColor(context, R.color.primary))
             .setSmallIcon(R.mipmap.notification_icon)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
@@ -59,7 +84,7 @@ class AppNotificationManager
 
     @RequiresApi(Build.VERSION_CODES.M)
     fun notifyNotificationPolicyAccessRequired() {
-        val notification = NotificationCompat.Builder(context)
+        val notification = NotificationCompat.Builder(context, ChannelIds.PERMISSION_NEEDED)
             .setColor(ContextCompat.getColor(context, R.color.primary))
             .setSmallIcon(R.mipmap.notification_icon)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
@@ -83,7 +108,7 @@ class AppNotificationManager
     }
 
     fun notifyCalendarPermissionRequired() {
-        val notification = NotificationCompat.Builder(context)
+        val notification = NotificationCompat.Builder(context, ChannelIds.PERMISSION_NEEDED)
             .setColor(ContextCompat.getColor(context, R.color.primary))
             .setSmallIcon(R.mipmap.notification_icon)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
@@ -104,6 +129,12 @@ class AppNotificationManager
     fun cancelCalendarPermissionRequired() {
         notificationManager.cancel(NotificationIds.CALENDAR_PERMISSION)
     }
+}
+
+object ChannelIds {
+    // use number prefix to control order in notifications settings
+    const val POLITE_ACTIVE = "1_polite_active"
+    const val PERMISSION_NEEDED = "2_permission_needed"
 }
 
 object NotificationIds {
