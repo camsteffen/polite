4890eeb4fcf03e320414d2084b16be1682ba653e
Cameron Steffen <cam.steffen94@gmail.com>
2019-08-24 18:02:00 -0500

    add AppPermissionChecker

---
+++ src/main/java/me/camsteffen/polite/util/AppPermissionChecker.kt
@@ -0,0 +1,34 @@
+package me.camsteffen.polite.util
+
+import android.Manifest.permission.READ_CALENDAR
+import android.content.Context
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
+import androidx.core.content.ContextCompat.checkSelfPermission
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppPermissionChecker
+@Inject constructor(
+    private val context: Context,
+    private val notificationManager: AppNotificationManager
+) {
+    fun checkReadCalendarPermission(): Boolean {
+        if (checkSelfPermission(context, READ_CALENDAR) != PERMISSION_GRANTED) {
+            notificationManager.notifyCalendarPermissionRequired()
+            return false
+        }
+        return true
+    }
+
+    fun checkNotificationPolicyAccess(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && !notificationManager.isNotificationPolicyAccessGranted
+        ) {
+            notificationManager.notifyNotificationPolicyAccessRequired()
+            return false
+        }
+        return true
+    }
+}
